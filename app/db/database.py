2d7d5f999a295627236359d7c18e3ca50389c768
Raya <rayakuzmina73@gmail.com>
2026-06-16 15:21:12 +0300

    fix: add DATABASE_URL checking

--- app/db/database.py
+++ app/db/database.py
@@ -6,5 +6,7 @@ from sqlalchemy import create_engine
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set")
 
 engine = create_engine(DATABASE_URL)
